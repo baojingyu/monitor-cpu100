090975441ab0d9a414f9e38647b7167aac2b5bda
baojingyu <31037754+baojingyu@users.noreply.github.com>
2024-01-18 14:58:59 +0800

    v2
    v2

--- monitor-cpu100-show-busy-java-threads.sh
+++ monitor-cpu100-show-busy-java-threads.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# 检查 show-busy-java-threads 脚本是否存在，如果不存在则下载并设置执行权限
-if [ ! -f show-busy-java-threads ]; then
-  curl -o show-busy-java-threads https://raw.githubusercontent.com/oldratlee/useful-scripts/dev-2.x/bin/show-busy-java-threads
-  chmod +x show-busy-java-threads
-fi
-
-# 设置监控时间间隔、阈值和重置时间间隔
+# 配置参数
 interval=5  # 监控时间间隔（秒）
 threshold=320  # CPU 使用率阈值（百分比）
-reset_interval=60  # 重置计数器的时间间隔（秒）
-message_interval=60  # 消息发送间隔（秒）
+threshold_message_push=400 # CPU 使用率阈值（百分比）,消息推送
+bucket_name="yt-nas"
+access_key="your-access-key"
+secret_key="your-secret-key"
+region="ap-east-1"
+# webhook_url="https://oapi.dingtalk.com/robot/send?access_token=23a63c41aa35939693d917df7da776826a1fa6a65ca44041a3aa20bd8c47dbdd"
+webhook_url="https://oapi.dingtalk.com/robot/send?access_token=49786f18c410e3a7aaf4c89ba30ff0be8844ae3360cc04b7bb928e18f6e16091"
 
-# 钉钉 Webhook URL
-webhook_url="https://oapi.dingtalk.com/robot/send?access_token=23a63c41aa35939693d917df7da776826a1fa6a65ca44041a3aa20bd8c47dbdd"
 
-# Elasticsearch配置
-# elasticsearch_uri="http://192.168.3.231:9200"
-# index_name="cpu_usage_logs"
+# 方法定义
+
+# 检查 show-busy-java-threads 脚本是否存在，如果不存在则下载并设置执行权限
+check_show_busy_java_threads() {
+  if [ ! -f show-busy-java-threads ]; then
+    curl -o show-busy-java-threads https://raw.githubusercontent.com/oldratlee/useful-scripts/dev-2.x/bin/show-busy-java-threads
+    chmod +x show-busy-java-threads
+  fi
+}
 
 # 获取Java应用的进程ID和主类
-# get_java_processes() {
-#   jps -l | awk '{print $1, $2}'
-# }
 get_java_processes() {
   pids=$(pgrep -f java)
   for pid in $pids
@@ -34,12 +34,38 @@ get_java_processes() {
   done
 }
 
-# 获取线程堆栈信息
+# 获取线程堆栈信息并上传至S3
 get_thread_stack_traces() {
   pid=$1
   # 从所有运行的Java进程中找出最消耗CPU的线程（前10个），打印出其线程栈
   jstack_output=$(./show-busy-java-threads -p $pid -c 10)
   echo "$jstack_output"
+}
+
+# 发送HTTP请求上传文件
+upload_file() {
+  file_path=$1 # 获取文件
+  file_name=$2 # 获取文件名
+
+  # 获取当前日期，格式为年月日
+  current_date=$(date +%Y%m%d)  
+  # 构建签名字符串
+  date=$(date -R)
+  content_type="application/octet-stream"
+  object_key="ops/thread_stack_traces/${current_date}/${file_name}"  # 设置对象的键，包括路径和文件名
+  string_to_sign="PUT\n\n${content_type}\n${date}\n/${bucket_name}/${object_key}"
+  signature=$(echo -en "${string_to_sign}" | openssl sha1 -hmac "${secret_key}" -binary | base64)
+  
+  # 上传
+  curl -X PUT -T "$file_path" \
+    -H "Host: ${bucket_name}.s3.${region}.amazonaws.com" \
+    -H "Date: ${date}" \
+    -H "Content-Type: ${content_type}" \
+    -H "Authorization: AWS ${access_key}:${signature}" \
+    "https://${bucket_name}.s3.${region}.amazonaws.com/${object_key}"
+
+  # 返回object_key
+  echo "$object_key"
 }
 
 # 发送钉钉消息
@@ -51,19 +77,18 @@ send_dingding_message() {
   curl "$webhook_url" -H 'Content-Type: application/json' -d "$data"
 }
 
-# 将日志写入Elasticsearch
-# write_to_elasticsearch() {
-#   log=$1
-#   curl -X POST -H "Content-Type: application/json" -d "$log" "$elasticsearch_uri/$index_name/_doc"
-# }
+# 主逻辑
+
+
+# 检查 show-busy-java-threads 脚本是否存在
+check_show_busy_java_threads
 
 # 获取所有Java应用的列表
 java_processes=$(get_java_processes)
 
-
 # 如果没有Java应用，则退出脚本
 if [ -z "$java_processes" ]; then
-  echo "No Java applications found. Exiting."
+  echo "未找到Java应用。退出脚本。"
   exit 1
 fi
 
@@ -73,101 +98,82 @@ pid=$(echo "$java_processes" | awk '/app\.jar/ {print $1}')
 # 如果没有找到名为 app.jar 的 Java 应用
 if [ -z "$pid" ]; then
   # 显示所有Java应用的列表
-  echo "Java applications found:"
+  echo "找到的Java应用程序："
   echo "$java_processes"
-  echo "Please enter the process ID of the Java application you want to monitor:"
+  echo "请输入要监视的Java应用程序的进程ID："
   read pid
 fi
 
 # 检查输入的进程ID是否存在
 if ! echo "$java_processes" | awk -v pid=$pid '{if ($1 == pid) exit 0; else exit 1}'; then
-  echo "Invalid process ID. Exiting."
+  echo "进程ID无效。正在退出。"
   exit 1
 fi
 
+# 获取容器IP
+container_ip=$(hostname -I | awk '{print $1}')
 
 # 获取应用名
 app_name=$(ps -ef | grep java | grep -o '\-Dskywalking\.agent\.service_name=[^ ]*' | cut -d'=' -f2)
 
 # 输出当前获取的应用名
-echo "Monitoring CPU usage of Java application: $app_name"
+echo "开始监控Java应用程序的CPU使用率情况：$app_name"
 
-# 初始化计数器和时间戳
-count=0
-last_alert_time=$(date +%s)
 
 # 循环监控
 while true
 do
-  # 获取当前时间（北京时间）
-  current_time=$(date +%s)
+  # 获取CPU使用率
+cpu_usage=$(top -b -n 1 -p $pid | awk '/^ *'$pid'/ {print $9}')
 
-  # 计算时间差
-  elapsed_time=$((current_time - last_alert_time))
-
-  # 如果时间差大于等于重置时间间隔，则重置计数器
-  if [ $elapsed_time -ge $reset_interval ]; then
-    count=0
-    last_alert_time=$current_time
-  fi
-
-  # 获取 CPU 使用率
-  cpu_usage=$(top -b -n 1 -p $pid | awk -v threshold=$threshold 'NR>7 { if ($1 == pid) { if ($9 >= threshold) print $9 } }' pid=$pid)
-
-  # 判断 CPU 使用率是否超过阈值
-  if [[ -n $cpu_usage ]]; then
-    if [ $count -lt 2 ]; then
-      echo "CPU usage of Java app is $cpu_usage%"
-
-      # 获取线程堆栈信息
-      thread_stack_traces=$(get_thread_stack_traces $pid)
-      echo "thread_stack_traces: $thread_stack_traces"
-
-      # 将线程堆栈输出到文件
-      output_file="jstack_output_$(date +"%Y%m%d%H%M%S%3N").txt"
-      echo "$thread_stack_traces" > $output_file
-      echo "Thread stack traces saved to $output_file"
+  # 判断CPU使用率是否超过阈值
+  if awk -v cpu_usage="$cpu_usage" -v threshold="$threshold" 'BEGIN { if (cpu_usage >= threshold) exit 1; else exit 0; }'; then
+    # CPU使用率超过阈值，输出线程堆栈信息并上传至S3
+    echo "Java应用程序$app_name（$pid）当前CPU使用率：($cpu_usage%)"
      
-      # 获取当前时间（北京时间，用于显示和日志）
-      display_time=$(TZ='Asia/Shanghai' date +"%Y-%m-%d %H:%M:%S.%3N")
-      
-      # 获取容器IP
-      container_ip=$(hostname -I | awk '{print $1}')
+    # 获取线程堆栈信息
+    thread_stack_traces=$(get_thread_stack_traces $pid)
 
+    # 打印线程堆栈信息
+    echo "打印线程堆栈信息: $thread_stack_traces"
+    
+    # 获取当前时间（北京时间，用于显示和日志）
+    display_time=$(TZ='Asia/Shanghai' date +"%Y-%m-%d %H:%M:%S.%3N")
+
+    # 构建文件头部内容
+    file_header_content="######################################################
+    # CPU Usage Alert
+    # 
+    # Current App Name: $app_name
+    # Current CPU Usage: $cpu_usage%
+    # Current Time: $display_time
+    # Container IP: $container_ip
+    ######################################################"    
+    
+    # 将线程堆栈输出到文件
+    output_file="${app_name}_jstack_output_$(date +"%Y%m%d%H%M%S%3N").txt"
+  
+    # 将文件头部内容和线程堆栈信息写入输出文件
+    echo "$file_header_content" > $output_file
+    echo "$thread_stack_traces" >> $output_file
+    echo "线程堆栈跟踪保存到： $output_file"
+
+    # 从输出文件路径中提取文件名
+    file_name="${output_file##*/}"
+    
+    # 上传线程堆栈信息文件到S3
+    uploaded_object_key=$(upload_file "$file_path" "$file_name")
+
+    # 判断CPU使用率是否超过另一个阈值（消息推送）
+    if awk -v cpu_usage="$cpu_usage" -v threshold="$threshold_message_push" 'BEGIN {if (cpu_usage >= threshold) exit 0; else exit 1}'; then
       # 转义特殊符号
       escaped_thread_stack_traces=$(echo "$thread_stack_traces" | sed 's/"/\\\"/g')
      
-      # 构建钉钉消息内容
-      message="CPU Usage Alert\n\nCPU usage of Java app is $cpu_usage%\n\nCurrent App Name: $app_name\n\nContainer IP: $container_ip\n\nCurrent Time: $display_time\n\nThread Stack Output File: $output_file\n\nThread Stack Traces (first 200 lines):\n\n$(echo "$escaped_thread_stack_traces" | head -n 200)"
-      
-      # 发送钉钉消息
+      # CPU使用率超过另一个阈值，发送钉钉提醒
+      message="CPU Usage Alert\n\nCPU usage of Java app is $cpu_usage%\n\nCurrent App Name: $app_name\n\nContainer IP: $container_ip\n\nCurrent Time: $display_time\n\nThread Stack Output File To S3 Object Key: $uploaded_object_key\n\nThread Stack Traces (first 50 lines):\n\n$(echo "$escaped_thread_stack_traces" | head -n 50)"
       send_dingding_message "$message"
-
-      # 构建日志数据
-      # log="{\"message\":\"$message\",\"threadStackTraces\":\"$escaped_thread_stack_traces\",\"cpuUsage\":\"$cpu_usage\",\"currentTime\":\"$display_time\"}"
-      
-      # escaped_thread_stack_traces_log=$(printf '%s' "$thread_stack_traces" | sed 's/\\/\\\\/g; s/"/\\"/g; s/\b/\\b/g; s/\f/\\f/g; s/\n/\\n/g; s/\r/\\r/g; s/\t/\\t/g')
-
-      # 构建日志数据
-      # log="{\"message\":\"$escaped_message\",\"threadStackTraces\":\"$escaped_thread_stack_traces_log\",\"cpuUsage\":\"$cpu_usage\",\"containerIP\":\"$container_ip\",\"currentTime\":\"$display_time\"}"
-
-      # 写入Elasticsearch日志
-      # write_to_elasticsearch "$log"
-
-      count=$((count + 1))
-      last_alert_time=$current_time
-    elif [ $count -eq 2 ]; then
-      # 获取当前时间与上一次发送消息的时间差
-      message_elapsed_time=$((current_time - last_alert_time))
-
-      # 如果时间差大于等于消息发送间隔，则重置计数器和上次发送消息的时间
-      if [ $message_elapsed_time -ge $message_interval ]; then
-        count=1
-        last_alert_time=$current_time
-      fi
     fi
   fi
 
-  # 等待指定时间间隔
   sleep $interval
 done
